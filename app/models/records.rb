64565784c2d38df5b478e2b382f54aa705771abf
eriko <eriko@19eafdee-8829-0410-94e2-88f925ca3ce2>
2008-05-02 22:25:13 +0000

    update missing model
    git-svn-id: https://svn.evergreen.edu:4443/repos/academic_computing/cal.evergreen.edu/weather_site/trunk@902 19eafdee-8829-0410-94e2-88f925ca3ce2

--- app/models/records.rb
+++ app/models/records.rb
@@ -1,2 +1,10 @@
 class Records < ActiveRecord::Base
+  
+  def Records.last_24_hours
+    end_time = Time.now
+    begin_time = Time.mktime(end_time.year,end_time.month,end_time.day,end_time.hour)-(86400)
+    Records.find(:all,:conditions => ["timestamp >= ? AND timestamp <= ?",begin_time.strftime("%Y-%m-%d %I:%M:%S %Z"),end_time.strftime("%Y-%m-%d %I:%M:%S %Z")])
+  end
+  
 end
+

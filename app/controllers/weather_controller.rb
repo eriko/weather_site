1180c42104d5bf88c7685b51834baae7122564ab
eriko <eriko@19eafdee-8829-0410-94e2-88f925ca3ce2>
2008-05-05 16:38:23 +0000

    open flash graph works
    git-svn-id: https://svn.evergreen.edu:4443/repos/academic_computing/cal.evergreen.edu/weather_site/trunk@909 19eafdee-8829-0410-94e2-88f925ca3ce2

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -5,7 +5,7 @@ class WeatherController < ApplicationController
   def index
     @record = Records.find(:first,:order => "timestamp DESC")
     @columns = [:timestamp , :record_num , :air_temp_c_avg , :air_temp_c_max , :air_temp_c_min , :rel_hum_avg , :rel_hum_max , :rel_hum_min , :wind_speed_ms_max , :wind_speed_avg , :wind_dir_avg , :solar_rad_w_avg , :solar_rad_w_max , :rain_mm_total , :dew_point_c_max , :dew_point_c_min , :wind_chill_c_max , :wind_chill_c_min , :heat_index_c_max , :heat_index_c_min , :etrs_mm_total , :rso]
-    @graph = open_flash_chart_object(600,300, '/weather/weather/today', true, '/weather')     
+    @graph = open_flash_chart_object(600,300, '/weather/weather/today', true, '/weather/')     
     @last_24 = Records.last_24_hours
   end
   

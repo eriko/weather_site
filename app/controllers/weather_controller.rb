d2ea716ec0b61213bfb0088e132a7591cbb0c2a6
eriko <eriko@19eafdee-8829-0410-94e2-88f925ca3ce2>
2008-05-06 18:38:46 +0000

    add more graphs for solar
    git-svn-id: https://svn.evergreen.edu:4443/repos/academic_computing/cal.evergreen.edu/weather_site/trunk@913 19eafdee-8829-0410-94e2-88f925ca3ce2

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -15,17 +15,27 @@ class WeatherController < ApplicationController
     render :template => "weather/graph"
   end
   
-  def week_temp
-    @graph = open_flash_chart_object(600,300, '/weather/weather/graph_temp/168', true, '/weather/')     
+  def two_day_temp
+    @graph = open_flash_chart_object(600,300, '/weather/weather/graph_temp/48', true, '/weather/')     
     render :template => "weather/graph"
   end
   
-    def week_solar
-    @graph = open_flash_chart_object(600,300, '/weather/weather/graph_solar/168', true, '/weather/')     
+    def two_day_solar
+    @graph = open_flash_chart_object(600,300, '/weather/weather/graph_solar/48', true, '/weather/')     
     render :template => "weather/graph"
   end    
   
     def day_solar
+    @graph = open_flash_chart_object(600,300, '/weather/weather/graph_solar/24', true, '/weather/')     
+    render :template => "weather/graph"
+  end
+  
+        def two_day_solar_max
+    @graph = open_flash_chart_object(600,300, '/weather/weather/graph_solar/48', true, '/weather/')     
+    render :template => "weather/graph"
+  end    
+  
+    def day_solar_max
     @graph = open_flash_chart_object(600,300, '/weather/weather/graph_solar/24', true, '/weather/')     
     render :template => "weather/graph"
   end
@@ -71,15 +81,56 @@ class WeatherController < ApplicationController
     g.set_swf_path('/weather/')
     g.set_js_path("/weather/javascripts/")
 
-    g.title( 'Solar Radiation', '{color: #7E97A6; font-size: 20; text-align: center}' )
+    g.title( 'Solar Radiation Average actual vs predicted', '{color: #7E97A6; font-size: 20; text-align: center}' )
     g.set_bg_color('#FFFFFF')
     g.set_data(last.collect {|record|record.solar_rad_w_avg})
-    g.line_dot( 2, 4, '#818D9D', 'Radiation in wm2', 10 )
-    g.set_y_max(4000)
+    g.line_dot( 2, 4, '#818D9D', 'Actual Radiation in watts/m2', 10 )
+    g.set_data(last.collect {|record|record.rso})
+    g.line_hollow( 2, 4, '#164166', 'Predicted Radiation in Megajoules/m²', 10 )
+    g.attach_to_y_right_axis(2)
+    g.set_y_max(3000)
+    g.set_y_right_max(10)
     g.set_x_axis_color('#818D9D', '#F0F0F0' )
     g.set_y_axis_color( '#818D9D', '#ADB5C7' )
+    g.y_right_axis_color('#164166' )
     g.set_x_legend( 'TESC Weather Station', 12, '#164166' )
-    g.set_y_legend( 'Solar Radiation Average', 12, '#164166' )
+    g.set_y_legend( 'Actual Average', 12, '#164166' )
+    g.set_y_legend_right( 'Predicted Average' ,12 , '#164166' )
+
+    g.set_x_labels(last.collect {|record|record.timestamp.strftime("%H:%M")})
+    g.set_x_label_style(10, '#164166', 0, 3, '#818D9D' )
+    g.set_y_label_steps(5)
+  
+    render :text => g.render
+  end
+  
+        def graph_solar
+    puts params
+
+    hours = params[:id].to_i
+    last = Records.last_x_hours(hours)
+    end_time = Time.now
+    begin_time = Time.mktime(end_time.year,end_time.month,end_time.day,end_time.hour)-(hours*3600) 
+  
+    g = Graph.new
+    g.set_swf_path('/weather/')
+    g.set_js_path("/weather/javascripts/")
+
+    g.title( 'Solar Radiation Maximum actual vs predicted', '{color: #7E97A6; font-size: 20; text-align: center}' )
+    g.set_bg_color('#FFFFFF')
+    g.set_data(last.collect {|record|record.solar_rad_w_max})
+    g.line_dot( 2, 4, '#818D9D', 'Max Radiation in watts/m2', 10 )
+    g.set_data(last.collect {|record|record.rso})
+    g.line_hollow( 2, 4, '#164166', 'Predicted Radiation in Megajoules/m²', 10 )
+    g.attach_to_y_right_axis(2)
+    g.set_y_max(3000)
+    g.set_y_right_max(10)
+    g.set_x_axis_color('#818D9D', '#F0F0F0' )
+    g.set_y_axis_color( '#818D9D', '#ADB5C7' )
+    g.y_right_axis_color('#164166' )
+    g.set_x_legend( 'TESC Weather Station', 12, '#164166' )
+    g.set_y_legend( 'Actual Average', 12, '#164166' )
+    g.set_y_legend_right( 'Predicted Average' ,12 , '#164166' )
 
     g.set_x_labels(last.collect {|record|record.timestamp.strftime("%H:%M")})
     g.set_x_label_style(10, '#164166', 0, 3, '#818D9D' )

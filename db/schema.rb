b1644bfaed87c37358f2de93c60ae4f9184279cf
eriko <eriko@19eafdee-8829-0410-94e2-88f925ca3ce2>
2008-05-09 18:47:36 +0000

    add sunrise and sun set data and display
    git-svn-id: https://svn.evergreen.edu:4443/repos/academic_computing/cal.evergreen.edu/weather_site/trunk@926 19eafdee-8829-0410-94e2-88f925ca3ce2

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 2) do
+ActiveRecord::Schema.define(:version => 3) do
 
   create_table "pg_ts_cfg", :id => false, :force => true do |t|
     t.text "ts_name",  :null => false
@@ -65,5 +65,10 @@ ActiveRecord::Schema.define(:version => 2) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "suns", :force => true do |t|
+    t.datetime "rise"
+    t.datetime "set"
+  end
 
 end
